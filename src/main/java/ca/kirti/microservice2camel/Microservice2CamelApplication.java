2cbda3f1cf705d2beca6cc56104d4bf31dda40c5
kirtimistry2001 <kirtimistry2001@yahoo.com>
2021-04-03 15:27:22 -0400

    Initial commit for Microservice2 using Camel with ActiveMQ to receive a message

---
+++ src/main/java/ca/kirti/microservice2camel/Microservice2CamelApplication.java
@@ -0,0 +1,13 @@
+package ca.kirti.microservice2camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice2CamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice2CamelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/kirti/microservice2camel/Microservice2CamelApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.kirti.microservice2camel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice2CamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
